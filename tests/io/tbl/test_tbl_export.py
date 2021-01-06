a4f6493f63e426cb5f7c7f2aa5fbcaee9a433f6b
Ian Fiddes <ian.fiddes@inscripta.com>
2021-01-06 12:07:17 -0800

    black

--- tests/io/tbl/test_tbl_export.py
+++ tests/io/tbl/test_tbl_export.py
@@ -10,18 +10,14 @@ from inscripta.biocantor.io.gff3.parser import parse_gff3_embedded_fasta
 from inscripta.biocantor.io.parser import ParsedAnnotationRecord
 
 
-@pytest.mark.parametrize("gff3,expected_tbl",
-                         [
-                             (
-                                "Inscripta_BL21_embedded_extra_contig.gff3", "Inscripta_BL21_embedded_extra_contig.tbl"
-                             ),
-                             (
-                                 "insO_frameshift.gff3", "insO_frameshift.tbl"
-                             ),
-                             (
-                                 "PEG10_offset_gff3_fasta.gff3", "PEG10_offset_gff3_fasta.tbl"
-                             )
-                         ])
+@pytest.mark.parametrize(
+    "gff3,expected_tbl",
+    [
+        ("Inscripta_BL21_embedded_extra_contig.gff3", "Inscripta_BL21_embedded_extra_contig.tbl"),
+        ("insO_frameshift.gff3", "insO_frameshift.tbl"),
+        ("PEG10_offset_gff3_fasta.gff3", "PEG10_offset_gff3_fasta.tbl"),
+    ],
+)
 def test_tbl_export_from_gff3(test_data_dir, tmp_path, gff3, expected_tbl):
     gff3 = test_data_dir / gff3
     recs = list(ParsedAnnotationRecord.parsed_annotation_records_to_model(parse_gff3_embedded_fasta(gff3)))
