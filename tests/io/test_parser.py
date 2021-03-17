f9b3397bfc21560762dad6d86b4d9fa789a609bb
Joshua Shorenstein <joshua.shorenstein@inscripta.com>
2021-03-16 22:08:50 -0600

    add test

---
+++ tests/io/test_parser.py
@@ -0,0 +1,48 @@
+from inscripta.biocantor.location.location_impl import SingleInterval
+from inscripta.biocantor.parent import Parent, SequenceType
+from inscripta.biocantor.sequence.alphabet import Alphabet
+from inscripta.biocantor.sequence.sequence import Sequence, Strand
+from inscripta.biocantor.io.parser import seq_chunk_to_parent
+
+
+def test_seq_chunk_to_parent():
+    obs = seq_chunk_to_parent("ATGCATGC", "TestSeq", 200, 208)
+    assert obs == Parent(
+        id="TestSeq:200-208",
+        sequence_type=SequenceType.SEQUENCE_CHUNK,
+        strand=None,
+        location=None,
+        sequence=Sequence(
+            data="ATGCATGC",
+            id="TestSeq:200-208",
+            alphabet=Alphabet.NT_EXTENDED_GAPPED,
+            type=SequenceType.SEQUENCE_CHUNK,
+            parent=Parent(
+                id="TestSeq",
+                sequence_type=SequenceType.CHROMOSOME,
+                strand=Strand.PLUS,
+                location=SingleInterval(
+                    200,
+                    208,
+                    Strand.PLUS,
+                    parent=Parent(
+                        id="TestSeq",
+                        sequence_type=SequenceType.CHROMOSOME,
+                        strand=Strand.PLUS,
+                        location=SingleInterval(200, 208, Strand.PLUS),
+                        sequence=None,
+                        parent=None,
+                    ),
+                ),
+                sequence=None,
+                parent=Parent(
+                    id="TestSeq",
+                    sequence_type=SequenceType.CHROMOSOME,
+                    strand=Strand.PLUS,
+                    location=SingleInterval(200, 208, Strand.PLUS, parent=None),
+                    sequence=None,
+                    parent=None,
+                ),
+            ),
+        ),
+    )
