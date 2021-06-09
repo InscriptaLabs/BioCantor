e7142d12f1470dc8737f5f0ae9ddeb33b346fbdd
Ian Fiddes <ian.fiddes@inscripta.com>
2021-06-08 21:41:50 -0700

    black

--- inscripta/biocantor/parent/parent.py
+++ inscripta/biocantor/parent/parent.py
@@ -49,8 +49,8 @@ class Parent:
         sequence_type: Optional[Union[SequenceType, str]] = None,
         strand: Optional[Strand] = None,
         # these cannot be imported due to circular import issues; I am not sure what to do about this
-        location: Optional["Location"] = None,  #noqa: F821
-        sequence: Optional["Sequence"] = None,  #noqa: F821
+        location: Optional["Location"] = None,  # noqa: F821
+        sequence: Optional["Sequence"] = None,  # noqa: F821
         parent: Optional[ParentInputType] = None,
     ):
         """
