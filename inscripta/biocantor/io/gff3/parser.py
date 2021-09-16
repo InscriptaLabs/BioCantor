1eb0f45b883699043abbffd2f72bcd0d18973e87
Maximilian Press <maximilianpress5@gmail.com>
2021-09-16 13:06:46 -0700

    black

--- inscripta/biocantor/io/gff3/parser.py
+++ inscripta/biocantor/io/gff3/parser.py
@@ -138,8 +138,12 @@ def _parse_genes(chrom: str, db: FeatureDB) -> List[Dict]:
     """
     parsed_genes = []
     for gene in db.region(
-        seqid=chrom, featuretype=[GFF3GeneFeatureTypes.GENE.value, GFF3GeneFeatureTypes.PSEUDOGENE.value,
-                                  GFF3GeneFeatureTypes.CDS.value]
+        seqid=chrom,
+        featuretype=[
+            GFF3GeneFeatureTypes.GENE.value,
+            GFF3GeneFeatureTypes.PSEUDOGENE.value,
+            GFF3GeneFeatureTypes.CDS.value,
+        ],
     ):
         parents = list(db.parents(gene))
         if len(parents) > 0:
