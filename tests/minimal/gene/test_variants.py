f6921ead1e16f33ff679efd230cf3bb2f9634a85
Ian Fiddes <ian.fiddes@inscripta.com>
2022-03-30 13:33:44 -0700

    more tests

--- tests/minimal/gene/test_variants.py
+++ tests/minimal/gene/test_variants.py
@@ -1,8 +1,9 @@
-from inscripta.biocantor.gene.variants import VariantInterval, VariantIntervalCollection
-from inscripta.biocantor.location import SingleInterval, Strand
-from inscripta.biocantor.sequence import Sequence, Alphabet
-from inscripta.biocantor.parent import Parent
 import pytest
+
+from inscripta.biocantor.exc import LocationOverlapException
+from inscripta.biocantor.gene.variants import VariantInterval, VariantIntervalCollection
+from inscripta.biocantor.parent import Parent
+from inscripta.biocantor.sequence import Sequence, Alphabet
 
 seq = Parent(sequence=Sequence("ACTCTCTCTATCTCATCCAC", Alphabet.NT_EXTENDED_GAPPED))
 snp_1 = VariantInterval(start=1, end=2, sequence="G", variant_type="SNV", parent_or_seq_chunk_parent=seq)
@@ -10,6 +11,8 @@ insertion_5 = VariantInterval(start=5, end=5, sequence="GG", variant_type="inser
 deletion_10_13 = VariantInterval(
     start=10, end=13, sequence="T", variant_type="deletion", parent_or_seq_chunk_parent=seq
 )
+deletion_12_13 = VariantInterval(start=12, end=13, sequence="", variant_type="deletion", parent_or_seq_chunk_parent=seq)
+deletion_13_15 = VariantInterval(start=13, end=15, sequence="", variant_type="deletion", parent_or_seq_chunk_parent=seq)
 
 
 class TestVariants:
@@ -19,6 +22,7 @@ class TestVariants:
             [snp_1, "AGTCTCTCTATCTCATCCAC"],
             [insertion_5, "ACTCTGGCTCTATCTCATCCAC"],
             [deletion_10_13, "ACTCTCTCTATCATCCAC"],
+            [deletion_13_15, "ACTCTCTCTATCTTCCAC"],
         ],
     )
     def test_edited_sequence(self, variant, expected):
@@ -32,8 +36,31 @@ class TestVariantCollections:
             [
                 VariantIntervalCollection([snp_1, insertion_5, deletion_10_13], parent_or_seq_chunk_parent=seq),
                 "AGTCTGGCTCTATCATCCAC",
-            ]
+            ],
+            [
+                VariantIntervalCollection(
+                    [snp_1, insertion_5, deletion_10_13, deletion_13_15], parent_or_seq_chunk_parent=seq
+                ),
+                "AGTCTGGCTCTATTCCAC",
+            ],
         ],
     )
     def test_edited_sequence(self, variant_collection, expected):
         assert str(variant_collection.edited_genomic_sequence) == expected
+
+    @pytest.mark.parametrize(
+        "variants,expected_exception",
+        [
+            [
+                [snp_1, snp_1],
+                LocationOverlapException,
+            ],
+            [
+                [deletion_10_13, deletion_12_13],
+                LocationOverlapException,
+            ],
+        ],
+    )
+    def test_exceptions(self, variants, expected_exception):
+        with pytest.raises(expected_exception):
+            _ = VariantIntervalCollection(variants)
