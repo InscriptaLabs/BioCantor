2de3d774b32d224f2c0e678c9ed81e8cf195201b
Ian Fiddes <ian.fiddes@inscripta.com>
2022-09-27 11:30:10 -0700

    asv initial tests

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,11 +1,25 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
-from inscripta.biocantor.io.genbank.parser import parse_genbank, ParsedAnnotationRecord
 from pathlib import Path
+
+from inscripta.biocantor.io.genbank.parser import parse_genbank, ParsedAnnotationRecord
+
+DATA_DIR = Path(__file__).parent.parent / "tests/data"
 
 
 class ParseGenBank:
-    params = ["tests/data/INSC1003.gbk", "tests/data/INSC1006_chrI.gbff", "tests/data/MG1655_subset.gbff", "tests/data/R64_subset.gbff"]
+    params = [
+        "INSC1003.gbk",
+        "INSC1006_chrI.gbff",
+        "MG1655_subset.gbff",
+        "R64_subset.gbff",
+    ]
 
-    def parse_genbank(self, gb):
-        _ = list(ParsedAnnotationRecord.parsed_annotation_records_to_model(parse_genbank(gb)))
+    def time_parse_genbank(self, gb):
+        _ = list(ParsedAnnotationRecord.parsed_annotation_records_to_model(parse_genbank(DATA_DIR / gb)))
+
+    def time_parse_genbank_translate(self, gb):
+        recs = list(ParsedAnnotationRecord.parsed_annotation_records_to_model(parse_genbank(DATA_DIR / gb)))
+        for rec in recs:
+            for gene in rec.genes:
+                _ = gene.get_primary_protein()
